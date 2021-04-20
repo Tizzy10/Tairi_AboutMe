e7d00a2a202fd791f952ac875cf24757966a5873
TizzJohnson <tizznasty10@gmail.com>
2021-04-20 08:37:10 -0400

    Initial Commit

---
+++ Tairi_AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tairi_AboutMe
+//
+//  Created by Tairi on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
